4483b08470a3cf31124504b0b9039b60a240e726
Tanvir Ahmad <tanvirengg@gmail.com>
2020-11-07 23:13:50 +0600

    update

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Login</title>
